d701fc8fdf001bdf08e9ee8c1c213bf0e852fca4
MichaelSilski <sil16005@byui.edu>
2022-03-24 11:07:15 -0600

    Moved jwt token string into env file

--- controllers/auth.js
+++ controllers/auth.js
@@ -71,7 +71,7 @@ exports.login = (req, res, next) => {
         email: loadedUser.email,
         userId: loadedUser._id.toString()
       },
-      'secretPrivateSignature',
+      ACCESS_TOKEN_SECRET,
       { expiresIn: '1h' }
     );
     res.status(200).json({ token: token, userId: loadedUser._id.toString() });
