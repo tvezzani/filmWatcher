6ed5c82b981efe4ab6993a2ca1b6a10edcfde377
MichaelSilski <sil16005@byui.edu>
2022-04-01 12:08:52 -0600

    Added logout and updated requests.http

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const User = require('../models/user');
 const {body} = require('express-validator')
 
 const authController = require('../controllers/auth');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -23,5 +24,23 @@ router.post('/signup',[
   ], authController.signup);
 
 router.post('/login', authController.login);
+
+router.put('/logout', function (req, res) {
+  const authHeader = req.headers["Authorization"];
+  jwt.sign(authHeader, "", { expiresIn: '1ms' }, (logout, err) => {
+    if (logout) {
+      res.status(201).json({message: 'You have been logged out'});
+    }
+    else {
+      const error = new Error('Error logging out.');
+      error.statusCode = 400;
+      throw error;
+    }
+  })
+  .catch((err) => {
+    err.statusCode = err.statusCode ? err.statusCode : 500;next(err);
+  });
+
+});
 
 module.exports = router;
